65931e7261632e1a5517756b72148fc009f953b5
ezekielkouassi <121268728+ezekielKouassi-dev@users.noreply.github.com>
2023-02-12 17:35:45 +0000

    first commit

---
+++ src/main/java/di/atos/dailychallengeweek7day2/DailyChallengeWeek7Day2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package di.atos.dailychallengeweek7day2;
+
+/**
+ *
+ * @author ezekielkouassi
+ */
+public class DailyChallengeWeek7Day2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
